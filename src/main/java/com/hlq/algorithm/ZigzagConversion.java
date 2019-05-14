724a81dbfbda587a7bf42ebe0e6129c522f99c4d
shdvgj <422409104@qq.com>
2019-05-14 18:30:36 +0800

    ZigzagConversion
    执行用时 : 20 ms, 在ZigZag Conversion的Java提交中击败了81.88% 的用户
    内存消耗 : 37.5 MB, 在ZigZag Conversion的Java提交中击败了98.61% 的用户

---
+++ src/main/java/com/hlq/algorithm/ZigzagConversion.java
@@ -0,0 +1,68 @@
+package com.hlq.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * 
+ 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+
+比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+
+L   C   I   R
+E T O E S I I G
+E   D   H   N
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+
+请你实现这个将字符串进行指定行数变换的函数：
+
+string convert(string s, int numRows);
+示例 1:
+
+输入: s = "LEETCODEISHIRING", numRows = 3
+输出: "LCIRETOESIIGEDHN"
+示例 2:
+
+输入: s = "LEETCODEISHIRING", numRows = 4
+输出: "LDREOEIIECIHNTSG"
+解释:
+
+L     D     R
+E   O E   I I
+E C   I H   N
+T     S     G
+ * @author Ricky
+ *
+ */
+public class ZigzagConversion {
+	public String convert(String s, int numRows) {
+		if (s.isEmpty()) return "";
+		if (numRows == 1) return s;
+		// 用于取余的数
+		int multiNum = (numRows + numRows - 2);
+		int len = s.length();
+		List<Character>[] lists = new ArrayList[numRows];
+		for(int i = 0; i < len; i++) {
+			int num = i % multiNum;
+			// 如果取余之后大于行数 ， 将该行数确定为1-4行中的一行
+			if (num + 1 > numRows) {
+				num = numRows - (num + 1 - numRows) - 1;
+			}
+			if (lists[num] == null) lists[num] = new ArrayList<Character>();
+			lists[num].add(s.charAt(i));
+		}
+		StringBuilder resultStr = new StringBuilder();
+		for (List<Character> list : lists) {
+			if (list == null || list.isEmpty()) break;
+			for (Character character : list) {
+				resultStr.append(character);
+			}
+		}
+		return resultStr.toString();
+	}
+	
+	public static void main(String[] args) {
+		ZigzagConversion zigzagConversion = new ZigzagConversion();
+		System.out.println(zigzagConversion.convert("A", 4));
+	}
+}
